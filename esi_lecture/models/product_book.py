0505d1f8b1ca62a901ff9d2db2e5342c7b01a229
54018 <54018@etu.he2b.be>
2021-12-08 10:45:58 +0100

    Modification of the product when init. WORKING NOW, addition of book into the product

--- esi_lecture/models/product_book.py
+++ esi_lecture/models/product_book.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     list_book_product = fields.Many2many('esi.lecture.livre', string="Liste de livres")
     list_book_count = fields.Integer(string='Nombre de livre', compute='_get_number_book', store=True)
 
+    @api.depends('list_book_product')
     def _get_number_book(self):
         for book in self:
             book.list_book_count = len(book.list_book_product)
