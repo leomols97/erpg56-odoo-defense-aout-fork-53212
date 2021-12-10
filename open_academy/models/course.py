80cf8334f73d447459d52bc82e61b7cf723ec711
54018 <54018@etu.he2b.be>
2021-12-10 08:46:13 +0100

    Deleted all unecessary directory not linked to project

--- open_academy/models/course.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class Course(models.Model):
-    _name = 'openacademy.course'
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
--- todo_stage/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import todo_task_tag_model
-from . import todo_task_model
